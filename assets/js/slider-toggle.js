f0da3e8cfd6811e27399bd9043942b9ae3b244d4
Dave Eargle <dave@daveeargle.com>
2021-08-20 22:46:49 -0600

    reduce dark-theme flashing somewhat

--- assets/js/slider-toggle.js
+++ assets/js/slider-toggle.js
@@ -1,37 +1,55 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const navbar = document.getElementById('navbar-main');
-
-const link_css_theme = document.getElementById('css-theme');
+// expects `dark_css_file` to be set elsewhere
+const dark_link_css_theme = document.getElementById('dark-css-theme');
+let toggleSwitch;
+let navbar;
 
 function _setDark() {
-  link_css_theme.href = dark_css_file;
-  navbar.classList.add('navbar-dark', 'bg-primary');
-  navbar.classList.remove('navbar-light', 'bg-light');
-  localStorage.setItem('theme', 'dark');
+  if (!dark_link_css_theme.href.includes(dark_css_file)) {
+    dark_link_css_theme.href = dark_css_file;
+  }
+  if (navbar) {
+    navbar.classList.add('navbar-dark', 'bg-primary');
+    navbar.classList.remove('navbar-light', 'bg-light');
+  }
 }
 
 function _setLight() {
-  link_css_theme.href = light_css_file;
-  navbar.classList.add('navbar-light', 'bg-light');
-  navbar.classList.remove('navbar-dark', 'bg-primary');
-  localStorage.setItem('theme', 'light');
+  dark_link_css_theme.href = "";
+  if (navbar) {
+    navbar.classList.add('navbar-light', 'bg-light');
+    navbar.classList.remove('navbar-dark', 'bg-primary');
+  }
 }
 
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-if (currentTheme) {
+function set_current_theme(){
+  const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+  if (currentTheme) {
     if (currentTheme === 'dark') {
       _setDark()
-      toggleSwitch.checked = true;
+      if (toggleSwitch) {
+        toggleSwitch.checked = true;
+      }
     }
+  }
 }
+// do it once so that it sets CSS before page loads
+set_current_theme();
 
 function switchTheme(e) {
     if (e.target.checked) {
+      localStorage.setItem('theme', 'dark');
       _setDark()
     }
     else {
+      localStorage.setItem('theme', 'light');
       _setLight()
     }
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+document.addEventListener("DOMContentLoaded", function() {
+  toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+  navbar = document.getElementById('navbar-main');
+  toggleSwitch.addEventListener('change', switchTheme, false);
+  // do it again so that it gets the nav this time
+  set_current_theme();
+});
