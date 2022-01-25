7441ed37b8b64223427b32d03ec15c5f6feb055f
Dave Eargle <dave@daveeargle.com>
2022-01-25 16:04:19 -0700

    use tocbot instead

--- js/script.ts
+++ js/script.ts
@@ -10,13 +10,29 @@ import 'bootstrap';
 
 import './slider-toggle';
 
-import './bootstrap-toc';
+import * as tocbot from 'tocbot';
 
 // window.Turbo = Turbo;
 window.Collapse = Collapse;
 
 const anchors = new AnchorJS();
 anchors.add('h1:not(.title), h2, h3, h4, h5, h6');
+
+document.addEventListener('DOMContentLoaded', function(){
+  tocbot.init({
+    // Where to render the table of contents.
+      tocSelector: '.js-toc',
+      // Where to grab the headings to build the table of contents.
+      contentSelector: '.js-toc-content',
+      // Which headings to grab inside of the contentSelector element.
+      headingSelector: 'h1, h2, h3',
+      positionFixedSelector: '.js-toc',
+      // For headings inside relative or absolute positioned containers within content.
+      hasInnerContainers: false,
+      // headingsOffset: 40,
+      // scrollSmoothOffset: -40,
+  })
+});
 
 // document.addEventListener('turbo:load', () => {
 //   anchors.add();
